b6753d2630247b34d1307807fc99a49e006c8ad9
Changyu-Guo <1028677200@qq.com>
2021-12-30 15:05:08 +0800

    0235.二叉搜索树的最近公共祖先

---
+++ tags/二叉树/二叉查找树/0235.二叉搜索树的最近公共祖先.py
@@ -0,0 +1,21 @@
+# -*- coding: utf - 8 -*-
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def lowest_common_ancestor(root, p, q):
+    while True:
+        if root.val > p.val and root.val > q.val:
+            root = root.left
+        elif root.val < p.val and root.val < q.val:
+            root = root.right
+        else:
+            return root
+
+
+if __name__ == '__main__':
+    pass
